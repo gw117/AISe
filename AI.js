8589ad4001c771253635f387f31b9135a0536fba
gw117 <44419923+gw117@users.noreply.github.com>
2018-10-24 16:45:35 +1100

    Added, self learning.

--- AI.js
+++ AI.js
@@ -1,5 +1,6 @@
 //concepts not psychology
 var output = "";
+var update = "";
 function OnStart()
 {
 	lay = app.CreateLayout( "linear", "VCenter,FillXY" );	
@@ -29,13 +30,18 @@ function btnLoad_OnTouch()
 
 
 while(0==0){
+
 var txt = app.ReadFile( "/sdcard/philosophy.txt" );
+
+
 var sentence = txt.split(".");
 
 x =  Math.floor(Math.random() * (sentence.length - 0 + 1)) + 0
 for(var x = 0;x < sentence.length;x++){
 var q = 0;
 var func = app.ReadFile( "/sdcard/parameters.txt" );
+
+
 var functionorder = func.split("\n");
 for (var c = x;c < sentence.length;c++){
 
@@ -49,6 +55,7 @@ var sent = sentence[c];
 var word = sent.split(" ");
 //a = b //functionorder
 x = c;
+
 if (q == 0){
 output += "";
 }
@@ -101,6 +108,13 @@ var word = sent.split(" ");
 x = c;
 if (q == 0){
 output += "";
+
+if (update.length > 1){
+var updater = app.ReadFile( "/sdcard/parameters.txt" );
+updater+= "\n" + update;
+app.WriteFile( "/sdcard/parameters.txt",updater );
+app.ShowPopup(update + " learned"  );
+}
 }
 q = 1;
 break;
@@ -181,6 +195,7 @@ for(var b = 0; b < vocab.length;b++){
 if(wordstr == vocab[b]){
 if (output.indexOf(wordstr) == -1){
 output += wordstr+" ";
+update = wordstr;
 edt.SetText(output);
 app.SetClipboardText( output);
 q = 1;
@@ -209,7 +224,7 @@ var word = sent.split(" ");
 //a = b //functionorder
 x = c;
 if (q == 0){
-output += "";
+output += ".";
 }
 q = 1;
 break;
