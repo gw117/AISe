83d695ca16ddadc009cccdab997b75c4aaa1e865
gw117 <44419923+gw117@users.noreply.github.com>
2018-11-27 09:09:58 +1100

    Cleaned up code for easy hardware integration, added comments

--- AI.js
+++ AI.js
@@ -17,89 +17,41 @@ var output2 = "";
 var semanticspread = 5
 function OnStart()
 {
-
-
 app.ShowPopup("initialising" );
-
-
 	lay = app.CreateLayout( "linear", "VCenter,FillXY" );	
-
-
-
-
 var output = "";
 var logicordershift = 1
-
-
 	edt = app.CreateTextEdit( "", 0.96, 0.7); 
 	edt.SetTextSize(8 );
 	lay.AddChild( edt );
-
 	edt2= app.CreateTextEdit( "", 0.96, 0.2); 
 	edt2.SetTextSize(8);
 	lay.AddChild( edt2 );
-
-
 	btnLoad = app.CreateButton( "Actuate", 0.23, 0.1 ); 
 	btnLoad.SetOnTouch( btnLoad_OnTouch ); 
 	lay.AddChild( btnLoad ); 
-
 	app.AddLayout( lay );
 }
-
-
 function btnLoad_OnTouch() 
 { 
-
  var db2 = "";
-
 app.ShowPopup("Construct" );
-
-
 var txt = app.ReadFile( "/sdcard/philosophy.txt" );
  txt += app.ReadFile( "/sdcard/AI.txt" );
 sentence = txt.split(".");
-
-
 while(0==0){
 output = "";
-
-
-
-
 for(var x = 0;x < sentence.length;x++){
-
+//line terminator/blocking, 
 terminator = output.split(" ");
-if (terminator.length > 20){
-if (stage == depth){
-x = pos-pos/5;
-stage = 0;
-output2 += ".\n";
-
-
- 
-
-
-sentence = txt.split(".");
-}
-
-//search and selector
-
-
-
-
-
+if (terminator.length > 20){//output length
 break;
 }
-
-stage++;
+//custom logic operator
 var q = 0;
 var func = app.ReadFile( "/sdcard/function.txt" );
-
-
 var functionorder = func.split("\n");
 for (var c = 0;c < sentence.length;c++){
-
 c =  Math.floor(Math.random() * (sentence.length)) + 0;
 var sent = sentence[c];
 var word = sent.split(" ");
@@ -112,7 +64,6 @@ var word = sent.split(" ");
 x = c;
 pos = c;
 x = Math.round(x);
-
 if (q == 0){
 output += "";
 }
@@ -128,12 +79,11 @@ if (q == 1){
 break
 }
 }
-
+//custom logic assembler
 var q = 0;
 var func = app.ReadFile( "/sdcard/noun.txt" );
 var functionorder = func.split("\n");
 for (var c = x;c < sentence.length;c++){
-
 c =  Math.floor(Math.random() * (sentence.length)) + 0;
 var sent = sentence[c];
 var word = sent.split(" ");
@@ -158,10 +108,7 @@ if (q == 1){
 break
 }
 }
-
-
-
-
+//custom logic operator
 var q = 0
 var sent = sentence[x];
 var word = sent.split(" ");
@@ -173,18 +120,14 @@ for(var b = 0; b < vocab.length;b++){
 if(wordstr == vocab[b]){
 if (output.indexOf(wordstr) == -1){
 if(a > word.length/difficulty){
-
-
 output += wordstr+" ";
-
 edt.SetText(output);
 }
-
 }
 }
 }
 }
-
+//custom logic assembler
 var q = 0;
 var sent = sentence[x];
 var word = sent.split(" ");
@@ -197,21 +140,17 @@ if(wordstr > vocab[b]){
 if (output.indexOf(wordstr) == -1){
 if(a > word.length*difficulty){
 output += wordstr+" ";
-
 edt.SetText(output);
 }
 }
 }
 }
 }
-
+//custom logic operator
 var q = 0;
-
-
 var func = app.ReadFile( "/sdcard/parameters.txt" );
 var functionorder = func.split("\n");
 for (var c = x;c < sentence.length;c++){
-
 c =  Math.floor(Math.random() * (sentence.length)) + 0;
 var sent = sentence[c];
 var word = sent.split(" ");
@@ -226,12 +165,10 @@ c = pos;
 x = Math.round(x);
 if (q == 0){
 output += "";
-
 if (update.length > 1){
 var updater = app.ReadFile( "/sdcard/parameters.txt" );
 updater+= "\n" + update;
-app.WriteFile( "/sdcard/parameters.txt",updater );
-
+app.WriteFile( "/sdcard/parameters.txt",updater );//logic operation updator
 }
 }
 q = 1;
@@ -246,16 +183,10 @@ if (q == 1){
 break
 }
 }
-
-
-
-
-
+//custom logic assembler
 var q = 0;
 for(var y = 0;y < sentence.length;y++){
 y =  Math.floor(Math.random() * (sentence.length - 0 + 1)) + 0
-
-
 var sent = sentence[y];
 var word = sent.split(" ");
 for(var a = 0;a < word.length;a++){
@@ -277,40 +208,26 @@ q = 1;
 }
 }
 }
-
 if (q == 1){
 break
 }
 }
-
-
 }
-
-
-
-
-
+//learning module
 var download = output.split(" ");
 var refreshermind = "";
 for (var mind = 0; mind < download.length;mind++){
 refreshermind+=  download[mind] + "\n";
 app.WriteFile( "/sdcard/function.txt",refreshermind,"append");
 }
-
 if (output.indexOf("?") > -1){
 output2+= ".";
-
 }
  output2 += output +  ".\n";
-
-
 app.WriteFile( "/sdcard/output.txt",output2, "append" );
-
  app.SetClipboardText( output2);
  edt2.SetText(output2);
  txt = output2;
-
-
 
 
 if (trial == 5){
@@ -321,7 +238,6 @@ var txt = app.ReadFile( "/sdcard/philosophy.txt" );
 
 
 if (trial == 10){
-
  txt = app.ReadFile( "/sdcard/output.txt" );
 app.ShowPopup("Proto emulation" ); //seperate proto emulation output db from construct output db? (for causation validation and interference protection, to prevent domain interchange between, new actuation and previous actuation(memory/brain and "hardware" cognition.)
 trial = 0;
@@ -338,19 +254,18 @@ trial++;
 	*/
 	
 	if (bound == 1){
-
-	
+//cyberneticLoop plugset
 for(var sentiencescan= 0;sentiencescan < sentence.length;sentiencescan++){
 var check = sentence[sentiencescan];
 var procarray = output2.split(" ");
-var cyberneticAlpha = procarray[procarray.length]
-var cyberneticBeta = procarray[procarray.length-1]
-var cyberneticCharlie = procarray[procarray.length-2]
+var cyberneticAlpha = procarray[procarray.length]//data node
+var cyberneticBeta = procarray[procarray.length-1]//data node
+var cyberneticCharlie = procarray[procarray.length-2]//data node
 nodeAlpha = check.indexOf(cyberneticAlpha);
 nodeBeta = check.indexOf(cyberneticBeta);
 nodeCharlie = check.indexOf(cyberneticCharlie);
-if (nodeAlpha == nodeBeta){
-if (nodeAlpha == nodeCharlie){
+if (nodeAlpha == nodeBeta){//plugset
+if (nodeAlpha == nodeCharlie){//plugset
 x = sentiencescan;
 }
 }
